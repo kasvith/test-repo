1d3302eab9df9a4bc629373703dadd5558a4b282
Kasun Vithanage <alankasun@gmail.com>
2017-09-03 12:38:41 +0530

    added message

---
+++ app/app/src/main/java/com/example/kasun/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kasun.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Comment
+        setContentView(R.layout.activity_main);
+    }
+}
